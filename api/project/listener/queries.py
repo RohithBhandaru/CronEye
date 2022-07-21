c23168bc04567964dcb7a8769fd97b216990c1ae
Rohith B <bhandarurohith@gmail.com>
2022-07-22 00:09:09 +0530

    Added scheduling and job event listener api

---
+++ api/project/listener/queries.py
@@ -0,0 +1,44 @@
+upsert_job = """
+INSERT INTO jobs (job_id, task)
+    VALUES (%s, %s)
+ON CONFLICT (job_id)
+    DO UPDATE SET
+        task = EXCLUDED.task
+"""
+
+update_job_activity = """
+UPDATE
+    jobs
+SET
+    is_active = %s
+WHERE
+    job_id = %s
+"""
+
+add_job_execution_event = """
+INSERT INTO events (job_id, status, time, return_value, exception, traceback)
+    VALUES (
+        (
+            SELECT
+                id
+            FROM
+                jobs
+            WHERE
+                job_id = %s
+        ), 
+        %s,
+        %s,
+        %s,
+        %s,
+        %s
+    )
+"""
+
+update_job_schedule = """
+UPDATE
+    jobs
+SET
+    next_scheduled_run = %s
+WHERE
+    job_id = %s
+"""
--- api/project/listener/routes.py
+++ api/project/listener/routes.py
@@ -1,11 +1,13 @@
-import logging
-from flask import jsonify
+import json, logging
+from flask import jsonify, request
+from apscheduler import events
 
 from . import listener
+from .queries import upsert_job, update_job_activity, add_job_execution_event, update_job_schedule
+from ..utils.db import DbConnection
 from ..utils.helper import authenticate
 from ..logs import config as logger_config
 from ..utils.enums import HTTPResponseCodes
-from ..utils.db import DbConnection
 from ..logs.logger_templates import (
     authentication_failed_401,
     bad_request_400,
@@ -33,7 +35,33 @@ def process_event(resp):
 
     conn, cur = DbConnection().get_db_connection_instance()
     try:
-        1
+        data = json.loads(request.data)
+        event_code = data.get("event_code")
+        job_id = data.get("job_id")
+        task = data.get("task")
+        status = data.get("status")
+        return_value = data.get("return_value")
+        exception = data.get("exception")
+        traceback = data.get("traceback")
+        next_scheduled_run_time = data.get("next_scheduled_run_time")
+        time = data.get("time")
+
+        if event_code == events.EVENT_JOB_ADDED:
+            cur.execute(upsert_job, (job_id, task))
+        if event_code == events.EVENT_JOB_REMOVED:
+            cur.execute(update_job_activity, (False, job_id))
+        if event_code == events.EVENT_JOB_SUBMITTED:
+            cur.execute(add_job_execution_event, (job_id, status, time, return_value, exception, traceback))
+            cur.execute(update_job_schedule, (next_scheduled_run_time, job_id))
+        if event_code in (events.EVENT_JOB_EXECUTED, events.EVENT_JOB_ERROR, events.EVENT_JOB_MISSED):
+            cur.execute(add_job_execution_event, (job_id, status, time, return_value, exception, traceback))
+        if event_code == events.EVENT_JOB_MAX_INSTANCES:
+            cur.execute(add_job_execution_event, (job_id, status, time, return_value, exception, traceback))
+            cur.execute(update_job_schedule, (next_scheduled_run_time, job_id))
+
+        conn.commit()
+        response["status"] = "success"
+        return jsonify(response), HTTPResponseCodes.SUCCESS
     except Exception:
         internal_server_error_500(logger, "POST", "/api/v1/listener/event", "")
         response["message"] = "Internal server error"
