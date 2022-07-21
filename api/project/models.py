9f9a970b8d003d78e59280ecb255cdb09a8df9f3
Rohith B <bhandarurohith@gmail.com>
2022-07-22 00:08:54 +0530

    Updated models

--- api/project/models.py
+++ api/project/models.py
@@ -8,22 +8,33 @@ class Jobs(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     job_id = db.Column(db.String, nullable=False)
     task = db.Column(db.String, nullable=False)
+    is_active = db.Column(db.BOOLEAN, nullable=False, default=True)
+    next_scheduled_run = db.Column(db.DateTime, default=datetime.now)
+
+    def __init__(self, job_id, task, is_active=True):
+        self.job_id = job_id
+        self.task = task
+        self.is_active = is_active
 
 
-class Events(db.Model):
-    __tablename__ = "events"
+class ExecutionEvents(db.Model):
+    __tablename__ = "execution_events"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     job_id = db.Column(db.Integer, db.ForeignKey("jobs.id"))
-    job = db.relationship("Events", backref=db.backref("jobs", uselist=True))
+    job = db.relationship("Jobs", backref=db.backref("execution_events", uselist=True))
     status = db.Column(db.String, nullable=False)
     time = db.Column(db.DateTime, default=datetime.now)
-    message = db.Column(db.Text, nullable=True)
+    return_value = db.Column(db.Text, nullable=True)
+    exception = db.Column(db.String, nullable=True)
+    traceback = db.Column(db.Text, nullable=True)
 
-    def __init__(self, job_id, status, start_time, message=""):
+    def __init__(self, job_id, status, start_time, return_value="", exception="", traceback=""):
         self.job_id = job_id
         self.status = status
         self.time = start_time
-        self.message = message
+        self.return_value = return_value
+        self.exception = exception
+        self.traceback = traceback
 
     def to_json(self):
         return {
@@ -31,5 +42,7 @@ class Events(db.Model):
             "task": self.job.task,
             "status": self.status,
             "time": self.time,
-            "message": self.message,
+            "return_value": self.return_value,
+            "exception": self.exception,
+            "traceback": self.traceback,
         }
