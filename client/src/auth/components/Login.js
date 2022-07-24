b5230605a895a2c06881dd4b2beffa8a211ceb0e
Rohith B <bhandarurohith@gmail.com>
2022-07-24 13:08:14 +0530

    Added login api call

--- client/src/auth/components/Login.js
+++ client/src/auth/components/Login.js
@@ -1,12 +1,21 @@
-import React from "react";
-import { useDispatch } from "react-redux";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
 
-import EmailIcon from "../../assets/icons/email.svg";
-import PasswordIcon from "../../assets/icons/password.svg";
+import EmailIcon from "../../assets/icons/EmailIcon.js";
+import PasswordIcon from "../../assets/icons/PasswordIcon.js";
+import { authFormSelector } from "../selectors/authSelector.js";
 
 import { updateAuthForm } from "../slice/authSlice";
 
+import config from "../../config/config.js";
+
 const Login = () => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [emailEmpty, setEmailEmpty] = useState(false);
+    const [passwordEmpty, setPasswordEmpty] = useState(false);
+
+    const formData = useSelector(authFormSelector);
     const dispatch = useDispatch();
     const handleChange = (e) => {
         dispatch(
@@ -18,15 +27,34 @@ const Login = () => {
         );
     };
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!formData.email || !formData.password) {
+            setEmailEmpty(!formData.email ? true : false);
+            setPasswordEmpty(!formData.password ? true : false);
+        } else {
+            setIsLoading(true);
+            setEmailEmpty(false);
+            setPasswordEmpty(false);
+            axios
+                .post(`${config.auth_base_url}/login`, { email: formData.email, password: formData.password })
+                .then((response) => {
+                    console.log(response.data);
+                });
+        }
+    };
+
     return (
         <div className="auth-main-container">
             <div className="auth-box-container">
                 <p className="auth-form-title">Login</p>
 
-                <form className="form__login" noValidate>
+                <form className="form__login" noValidate onSubmit={handleSubmit}>
                     <div className="form-field-title-div form-field-title">Email</div>
                     <div className="form-field-box form-input">
-                        <img src={EmailIcon} alt="Email" className="auth-icons" align="middle" />
+                        <div className="auth-icons">
+                            <EmailIcon color={emailEmpty ? "red" : "black"} width={23} height={23} />
+                        </div>
                         <input className="form-input" name="email" type="text" required onChange={handleChange} />
                     </div>
 
@@ -34,7 +62,9 @@ const Login = () => {
                         <div className="form-field-title">Password</div>
                     </div>
                     <div className="form-field-box form-input">
-                        <img src={PasswordIcon} alt="Password" className="auth-icons" align="middle" />
+                        <div className="auth-icons">
+                            <PasswordIcon color={passwordEmpty ? "red" : "black"} width={23} height={23} />
+                        </div>
                         <input
                             className="form-input"
                             name="password"
---
+++ client/src/config/config.js
@@ -0,0 +1,6 @@
+const config = {
+    base_url: "http://127.0.0.1:5000/",
+    auth_base_url: "http://127.0.0.1:5000/api/auth",
+};
+
+export default config;
