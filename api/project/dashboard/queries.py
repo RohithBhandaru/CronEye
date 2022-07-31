5e21af1b5f304a329419176d631209e2c34fc10a
Rohith B <bhandarurohith@gmail.com>
2022-07-31 13:59:59 +0530

    Added jobs summary api

--- api/project/dashboard/queries.py
+++ api/project/dashboard/queries.py
@@ -16,3 +16,27 @@ ORDER BY
     bb.job_id,
     cc.time DESC
 """
+
+job_stats = """
+SELECT
+    bb.job_id,
+    bb.task,
+    bb.next_scheduled_run,
+    cc.time,
+    cc.status,
+    cc.return_value,
+    cc.exception,
+    cc.traceback
+FROM
+    schedulers aa
+    LEFT JOIN jobs bb ON aa.id = bb.scheduler_id
+    LEFT JOIN execution_events cc ON bb.id = cc.job_id
+WHERE
+    aa.status = 'active'
+    AND bb.is_active IS TRUE
+    AND cc.time >= '%s'
+    AND cc.time <= '%s'
+ORDER BY
+    bb.job_id,
+    cc.time ASC
+"""
--- api/project/dashboard/routes.py
+++ api/project/dashboard/routes.py
@@ -1,8 +1,8 @@
-import json, logging
+import json, logging, pandas as pd, datetime as dt
 from flask import jsonify, request
 
 from . import dashboard
-from .queries import summary_stats
+from .queries import summary_stats, job_stats
 from ..utils.db import DbConnection
 from ..utils.helper import authenticate_user
 from ..logs.config import config as logger_config
@@ -21,7 +21,7 @@ def summary(resp):
         "message": "",
         "data": {"scheduler": "inactive", "total_jobs": 0, "running_jobs": 0, "overrun_jobs": 0},
     }
-    conn, cur = DbConnection().get_db_connection_instance()
+    _, cur = DbConnection().get_db_connection_instance()
 
     try:
         # Data - Scheduler status, total jobs in the jobstore, running jobs, overruning jobs
@@ -40,6 +40,96 @@ def summary(resp):
         response["status"] = "success"
         return jsonify(response), HTTPResponseCodes.SUCCESS.value
     except Exception:
-        internal_server_error_500(logger, "POST", "/api/listener/event", "", {})
+        internal_server_error_500(logger, "POST", "/api/dashboard/summary", "", {})
+        response["message"] = "Internal server error"
+        return jsonify(response), HTTPResponseCodes.INTERNAL_SERVER_ERROR.value
+
+
+@dashboard.route("/summary/jobs", methods=["POST"])
+@authenticate_user
+def jobs_summary(resp):
+    "Assuming only one instance of a job can run at a time"
+    response = {
+        "status": "failure",
+        "message": "",
+        "data": {"jobs": []},
+    }
+    conn, _ = DbConnection().get_db_connection_instance()
+    try:
+        start_time = dt.datetime(
+            year=2022, month=7, day=26, hour=0, minute=0, second=0
+        )  # dt.datetime.utcnow() + dt.timedelta(hours=-6)
+        end_time = dt.datetime.utcnow()
+        data = pd.read_sql(job_stats % (start_time, end_time), conn)
+        unique_jobs = data["job_id"].unique()
+        for job in unique_jobs:
+            events = []
+            sub_data = data[data["job_id"] == job]
+            current_event_idx = -1
+            for _, datum in sub_data.iterrows():
+                if datum["status"] == "EVENT_JOB_SUBMITTED":
+                    current_event_idx += 1
+                    events.append(
+                        {
+                            "start": datum["time"],
+                            "end": None,
+                            "return_value": None,
+                            "exception": None,
+                            "traceback": None,
+                            "miss_flag": False,
+                            "max_instance_count": [],
+                        }
+                    )
+
+                if datum["status"] in ("EVENT_JOB_EXECUTED", "EVENT_JOB_ERROR", "EVENT_JOB_MAX_INSTANCES"):
+                    if current_event_idx >= 0:
+                        if datum["status"] != "EVENT_JOB_MAX_INSTANCES":
+                            events[current_event_idx]["end"] = datum["time"]
+                            events[current_event_idx]["return_value"] = datum["return_value"]
+                            events[current_event_idx]["exception"] = datum["exception"]
+                            events[current_event_idx]["traceback"] = datum["traceback"]
+                        else:
+                            events[current_event_idx]["max_instance_count"].append(datum["time"])
+                    else:
+                        current_event_idx += 1
+                        events.append(
+                            {
+                                "start": None,
+                                "end": datum["time"] if datum["status"] != "EVENT_JOB_MAX_INSTANCES" else None,
+                                "return_value": datum["return_value"],
+                                "exception": datum["exception"],
+                                "traceback": datum["traceback"],
+                                "miss_flag": False,
+                                "max_instance_count": [datum["time"]],
+                            }
+                        )
+
+                if datum["status"] == "EVENT_JOB_MISSED":
+                    current_event_idx += 1
+                    events.append(
+                        {
+                            "start": datum["time"],
+                            "end": None,
+                            "return_value": None,
+                            "exception": None,
+                            "traceback": None,
+                            "miss_flag": True,
+                            "max_instance_count": [],
+                        }
+                    )
+
+            response["data"]["jobs"].append(
+                {
+                    "id": sub_data.iloc[0, 0],
+                    "name": sub_data.iloc[0, 1],
+                    "next_scheduled_run": sub_data.iloc[0, 2],
+                    "events": events,
+                }
+            )
+
+        response["status"] = "success"
+        return jsonify(response), HTTPResponseCodes.SUCCESS.value
+    except Exception:
+        internal_server_error_500(logger, "POST", "/api/dashboard/summary/jobs", "", {})
         response["message"] = "Internal server error"
         return jsonify(response), HTTPResponseCodes.INTERNAL_SERVER_ERROR.value
