a1c23ff8091c39598beafe9051e4945b1aa97ffa
Rohith B <bhandarurohith@gmail.com>
2022-10-01 13:01:06 +0530

    Added no data view

--- client/src/dashboard/components/LogsList.js
+++ client/src/dashboard/components/LogsList.js
@@ -3,6 +3,9 @@ import { connect } from "react-redux";
 
 import LogHeader from "./LogHeader";
 import LogAccordian from "./LogAccordian";
+import Paginator from "./Paginator";
+
+import NoDataIllustration from "../../assets/illustrations/NoDataIllustration";
 
 // Accessible part of the state
 const mapStateToProps = (state) => {
@@ -12,20 +15,33 @@ const mapStateToProps = (state) => {
 };
 
 const LogsList = (props) => {
-    console.log(props.logs);
-
+    console.log(props.logs.length);
     return (
         <div className="logs-container">
-            {props.logs?.length > 1 ? (
-                <div className="table-container">
-                    <LogHeader />
-                    {props.logs.map((log) => (
-                        <LogAccordian {...log} />
-                    ))}
-                </div>
-            ) : (
-                <div>1</div>
-            )}
+            <div className="table-container">
+                <LogHeader />
+                {props.logs?.length > 1 ? (
+                    props.logs.map((log) => <LogAccordian {...log} />)
+                ) : (
+                    <div
+                        style={{
+                            display: "flex",
+                            flexDirection: "column",
+                            justifyContent: "center",
+                            alignItems: "center",
+                            padding: "20px",
+                            width: "100%",
+                            backgroundColor: "white",
+                        }}
+                    >
+                        <NoDataIllustration width="200" height="200" />
+                        <span style={{ margin: "20px", color: "#0a64a5", fontSize: "14px", fontWeight: "600" }}>
+                            No data available
+                        </span>
+                    </div>
+                )}
+                <Paginator />
+            </div>
         </div>
     );
 };
